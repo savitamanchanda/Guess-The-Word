a715203aa432b26517d62f3e3b6f24b337e4b622
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2022-11-30 13:29:15 -0600

    added more functions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var timerEl = document.getElementById('countdown');
 var wordBlank = document.querySelector(".word-blank");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".loss");
+var resetButton = document.querySelector(".reset");
 
 var words = ["javascript", "function", "merge", "branch","element"]
 
@@ -11,6 +12,8 @@ var letters = [];
 var numLetters = 0; 
 var blankLetters = [];
 var isWin = false;
+var winCounter = 0;
+var loseCounter = 0;
 
 
 
@@ -22,22 +25,71 @@ function startTimer(){
       timeLeft--;
       timerEl.textContent = timeLeft + " seconds remaining";
   
-      if((timeLeft ===0)) {
- 
+      if((timeLeft >= 0)) {
+        if (isWin && timeLeft > 0) {
+            clearInterval(timeInterval);
+            winGame();
+        }
+      }
+
+      if (timeLeft === 0) {
         clearInterval(timeInterval);
-    
+        loseGame();
       }
   
     }, 1000);
     
 };
 
+function winGame() {
+    wordBlank.textContent = "Congratulation! You won!"
+    winCounter++
+    setWins();
+}
+
+function loseGame() {
+    wordBlank.textContent = "Sorry! You lost!"
+    loseCounter++
+    setLoses();
+}
+
+function setWins() {
+    win.textContent = winCounter
+    localStorage.setItem("winCount", winCounter);
+}
+
+function setLoses() {
+    lose.textContent = loseCounter
+    localStorage.setItem("loseCount", loseCounter);
+}
+
+function getWins() {
+    var storedWins = localStorage.getItem("winCount");
+    
+    if (storedWins === null){
+        winCounter = 0;
+    } else {
+        winCounter = storedWins;
+    }
+    win.textContent = winCounter;
+}
+
+function getLoses() {
+    var storedLoses = localStorage.getItem("loseCount")
+
+    if (storedLoses === null){
+        loseCounter = 0;
+    } else {
+        storedLoses = loseCounter
+    }
+    lose.textContent = loseCounter;
+}
+
 function startGame() { 
 isWin = false;
 renderBlanks();
 startTimer();
 }
-
 
 
 function renderBlanks() {
@@ -52,5 +104,41 @@ function renderBlanks() {
     wordBlank.textContent = blankLetters.join(" ")
 }
 
+function checkLetters(letter) {
+    var letterInWord = false; 
+    for (var i =0; i < numLetters; i++) {
+        if (chosenWord[i] === letter) {
+            letterInWord = true;
+            blankLetters[i] = letter
+        }
+    }
+    wordBlank.textContent = blankLetters.join(" ")
+}
+document.addEventListener("keydown", function(event){
+    var key = event.key.toLowerCase();
+    var characters = "abcdefghijklmnopqrstuvwxyz".split("");
 
- startButton.addEventListener("click", startGame());     
+    if (characters.includes(key)) {
+        var guessedWord = event.key;
+        checkLetters(guessedWord);
+        checkWin();
+    }
+})
+
+function checkWin (){ 
+    if (chosenWord === blankLetters.join("")){
+        isWin = true;
+    }
+}
+ 
+startButton.addEventListener("click", startGame());     
+
+function resetGame() {
+    winCounter = 0;
+    loseCounter = 0;
+
+    setWins()
+    setLoses()
+}
+
+resetButton.addEventListener("click", resetGame());
