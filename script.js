eca9426a27541be6cf14988b975fd3eec07da0d5
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2022-11-30 12:47:16 -0600

    added functions to start the game

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var startGame = document.getElementById('start');
+var startButton = document.getElementById('start');
 var timerEl = document.getElementById('countdown');
+var wordBlank = document.querySelector(".word-blank");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".loss");
 
-startGame.addEventListener("click", function(){
+var words = ["javascript", "function", "merge", "branch","element"]
+
+var chosenWord = "";
+var letters = [];
+var numLetters = 0; 
+var blankLetters = [];
+var isWin = false;
+
+
+
+function startTimer(){
 
     var timeLeft = 10;
 
@@ -9,7 +22,7 @@ startGame.addEventListener("click", function(){
       timeLeft--;
       timerEl.textContent = timeLeft + " seconds remaining";
   
-      if(timeLeft === 0) {
+      if((timeLeft ===0)) {
  
         clearInterval(timeInterval);
     
@@ -17,5 +30,27 @@ startGame.addEventListener("click", function(){
   
     }, 1000);
     
-});
-      
+};
+
+function startGame() { 
+isWin = false;
+renderBlanks();
+startTimer();
+}
+
+
+
+function renderBlanks() {
+    chosenWord = words[Math.floor(Math.random() * words.length)];
+    letters = chosenWord.split("");
+    numLetters = letters.length;
+    blankLetters = [];
+
+    for (var i = 0; i < numLetters; i++) {
+        blankLetters.push("_");
+    }
+    wordBlank.textContent = blankLetters.join(" ")
+}
+
+
+ startButton.addEventListener("click", startGame());     
